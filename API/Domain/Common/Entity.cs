ce177638c66f04c179c59e1275c884a4c4b1e815
VictoRGBC <victorgabtec@gmail.com>
2026-03-01 22:45:33 -0300

    Criacao do Domain

---
+++ API/Domain/Common/Entity.cs
@@ -0,0 +1,6 @@
+﻿public abstract class Entity
+{
+    public Guid Id { get; protected set; } = Guid.NewGuid();
+    public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; protected set; }
+}
---
+++ API/Domain/Entities/Account.cs
@@ -0,0 +1,23 @@
+﻿using System.Transactions;
+
+public class Account : Entity
+{
+    public string Name { get; private set; }
+    public string Type { get; private set; }
+    public Guid UserId { get; private set; }
+
+    // Coleção privada para garantir o encapsulamento (Princípio de OCP)
+    private readonly List<Transaction> _transactions = new();
+    public IReadOnlyCollection<Transaction> Transactions => _transactions.AsReadOnly();
+
+    public Account(string name, string type, Guid userId)
+    {
+        // Aqui aplicaríamos Domain Validations (SRP)
+        Name = name;
+        Type = type;
+        UserId = userId;
+    }
+
+    // O Saldo é calculado, não armazenado
+    public decimal GetBalance() => _transactions.Sum(t => t.Amount);
+}
